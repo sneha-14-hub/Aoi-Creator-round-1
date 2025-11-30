205c31b3d2abbd9b42dcb636901abd6192bd93fc
sneha_dubey <yourselflove9760@gmail.com>
2025-11-30 23:03:25 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Map from './components/map/Map.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Map />
+    </div>
+  );
+}
+
+export default App;
